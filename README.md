998ed50346a5026e4be26451a9ccedf3cd9782a7
flux <25628292+fluxionary@users.noreply.github.com>
2023-03-11 20:22:45 -0800

    initial

---
+++ README.md
@@ -0,0 +1,47 @@
+# item_monoids
+
+an API for creating monoids for items. useful for
+* enchantment mods
+* applying penalties or boosts to dig speed and such, based on player attributes or other systems
+* when modifying other metadata (e.g. the description) of an item.
+
+### API
+
+create a tool monoid:
+
+```lua
+local full_punch_monoid = item_monoids.make_monoid("full_punch", {
+    predicate = function(itemstack) -- [optional] does this monoid apply to this stack?
+        local def = itemstack:get_definition()
+        return def and def.tool_capabilities and def.tool_capabilities.full_punch_interval
+    end,
+    get_default = function(itemstack) -- [optional] get the default value (otherwise nil)
+        return itemstack:get_definition().tool_capabilities.full_punch_interval
+    end,
+    fold = function(values, default) -- combine the values
+        local value = default
+        for _, other_value in pairs(values) do
+            value = value * other_value
+        end
+        return value
+    end,
+    apply = function(full_punch_interval, itemstack) -- [optional] apply the value to the stack
+        local tool_capabilities = itemstack:get_tool_capabilities()
+        tool_capabilities.full_punch_interval = full_punch_interval
+        local meta = itemstack:get_meta()
+        meta:set_tool_capabilities(tool_capabilities)
+    end,
+})
+
+local player
+local itemstack = player:get_wielded_item()
+if should_slow_punch(player) then
+    full_punch_monoid:add_change(itemstack, 4, "punch_penalty")  -- increases the full punch interval 4x
+else
+    full_punch_monoid:del_change(itemstack, "punch_penalty")
+end
+```
+
+#### pitfalls
+
+* be careful not to modify the *definition* of a tool, just tool metadata.
---
+++ init.lua
@@ -0,0 +1,76 @@
+item_monoids = fmod.create()
+
+local f = string.format
+
+local ItemMonoid = futil.class1()
+
+function ItemMonoid:_init(name, def)
+	self._name = name
+	self._predicate = def.predicate
+	self._get_default = def.get_default
+	self._fold = def.fold
+	self._apply = def.apply
+end
+
+function ItemMonoid:_get_values(meta)
+	local key = f("tm:%s", self._name)
+	return minetest.deserialize(meta:get(key)) or {}
+end
+
+function ItemMonoid:_set_values(meta, values)
+	local key = f("tm:%s", self._name)
+	meta:set_string(key, minetest.serialize(values))
+end
+
+function ItemMonoid:add_change(itemstack, value, id)
+	if self._predicate and not self._predicate(itemstack) then
+		return
+	end
+	local meta = itemstack:get_meta()
+	local values = self._get_values(meta)
+	values[id] = value
+	self._set_values(meta, values)
+	local default
+	if self._get_default then
+		default = self._get_default(itemstack)
+	end
+	local folded = self._fold(values, default)
+	if self._apply then
+		self._apply(folded, itemstack)
+	end
+	return folded
+end
+
+function ItemMonoid:del_change(itemstack, id)
+	if self._predicate and not self._predicate(itemstack) then
+		return
+	end
+	local meta = itemstack:get_meta()
+	local values = self._get_values(meta)
+	values[id] = nil
+	self._set_values(meta, values)
+	local default
+	if self._get_default then
+		default = self._get_default(itemstack)
+	end
+	local folded = self._fold(values, default)
+	if self._apply then
+		self._apply(folded, itemstack)
+	end
+	return folded
+end
+
+function ItemMonoid:value(itemstack)
+	if self._predicate and not self._predicate(itemstack) then
+		return
+	end
+	local meta = itemstack:get_meta()
+	local values = self._get_values(meta)
+	local default
+	if self._get_default then
+		default = self._get_default(itemstack)
+	end
+	return self._fold(values, default)
+end
+
+item_monoids.make_monoid = ItemMonoid
